92e9950341e8661f9c55554216160d7406afed58
tbaudeau <thomas.baudeau.33@gmail.com>
2023-07-19 11:51:33 +0200

    changescript

--- bigrun.sh
+++ bigrun.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-var1=/home/baudeau/Documents/BenchMapL/ProjetSuede/sGenerate/result/final_COV_agregate.fastq
-a= pwd
+var1=../Tool/sgENERATE/result
+var2=../../datasets/
 for f in *;
 	do mv $f 'final_COV_agregate.fastq.gz';
 	mv 'final_COV_agregate.fastq.gz' $var1 ;
 	conda activate Snakemake;
-	cd /home/baudeau/Documents/BenchMapL/ProjetSuede/sGenerate/;
+	cd ../Tool/sgENERATE/;
 	snakemake -c8 all --use-conda;
 	rm -r result;
 	mkdir result;
@@ -16,7 +16,7 @@ for f in *;
 	tail -n+2 Periscope_mult/COV_periscope_novel_counts.csv >> finalresult/peri2/COV_periscope_novel_counts.csv;
 	rm -r Periscope;
 	rm -r Periscope_mult;
-	cd $a
+	cd $var2
 
 
 
