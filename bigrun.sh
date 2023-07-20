07a5069cce87f0162fe2d36def90e4635a5bb613
tbaudeau <thomas.baudeau.33@gmail.com>
2023-07-20 11:15:01 +0200

    minor fix

--- bigrun.sh
+++ bigrun.sh
@@ -2,13 +2,15 @@
 
 var1=../Tool/sgENERATE/result
 var2=../../datasets/
+
+
+conda activate Snakemake;
 for f in *;
 	do mv $f 'final_COV_agregate.fastq.gz';
 	mv 'final_COV_agregate.fastq.gz' $var1 ;
-	conda activate Snakemake;
 	cd ../Tool/sgENERATE/;
-	snakemake -c8 all --use-conda;
-	rm -r result;
+	snakemake -c16 all --use-conda || true;
+	rm -rf result;
 	mkdir result;
 	mv benchmarks/PeriscopeMult_COV.txt benchmarks/PeriscopeMult_COV_$f.txt;
 	mv benchmarks/Periscope_COV.txt benchmarks/Periscope_COV_$f.txt;
@@ -16,11 +18,8 @@ for f in *;
 	tail -n+2 Periscope/COV_periscope_novel_counts.csv >> finalresult/peri/COV_periscope_novel_counts.csv;
 	tail -n+2 Periscope_mult/COV_periscope_counts.csv >> finalresult/peri2/COV_periscope_counts.csv;
 	tail -n+2 Periscope_mult/COV_periscope_novel_counts.csv >> finalresult/peri2/COV_periscope_novel_counts.csv;
-	rm -r Periscope;
-	rm -r Periscope_mult;
+	rm -rf Periscope;
+	rm -rf Periscope_mult;
 	cd $var2;
-
-
-
 done
 
